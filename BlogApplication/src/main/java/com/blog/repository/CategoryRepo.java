e8ed64029cc3cbf93d50ef5fd2ca857074b487dd
Sushant Narvekar <115461235+Sushant2518@users.noreply.github.com>
2024-04-18 14:51:36 +0530

    Delete BlogApplication directory

--- BlogApplication/src/main/java/com/blog/repository/CategoryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.blog.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.blog.entities.Category;
-
-public interface CategoryRepo extends JpaRepository<Category, Integer> {
-
-}
